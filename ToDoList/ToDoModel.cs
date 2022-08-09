27ffec2553e7d60274858464f7a0a3549dd7169e
kravchenkoegorii <kravchenko13138@gmail.com>
2022-08-09 14:48:08 +0300

    code clean up

--- ToDoList/ToDoModel.cs
+++ ToDoList/ToDoModel.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ToDoList
+﻿namespace ToDoList
 {
     public class ToDoModel
     {
         public int Id { get; set; }
-        public string Task { get;set; }
-        public DateTime StartDate { get; set; } 
+        public string Task { get; set; }
+        public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public override string ToString()
         {
